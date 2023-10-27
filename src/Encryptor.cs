86beadfddbbadaf5fd08a2832ff9047fa14f483a
nyansii <134614081+nyansii@users.noreply.github.com>
2023-10-27 15:51:25 +0900

    Changed to a method that does not use async. Other small fixes.

--- src/Encryptor.cs
+++ src/Encryptor.cs
@@ -1,79 +1,100 @@
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 
 namespace FileEncryptor;
 
 public class Encryptor
 {
-    /// <summary>
-    /// Encrypt the file of "inputFilePath" and output to the location of "outputFilePath".
-    /// </summary>
-    /// <param name="inputFilePath"></param>
-    /// <param name="outputFilePath"></param>
-    /// <returns>Returns true if the encryption was successful.</returns>
-    public static async Task<bool> Encrypt(string key, string inputFilePath, string outputFilePath)
-    {
-        try
-        {
-            using var inputFileStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
-            using var outputFileStream = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write);
-            using var aes = Aes.Create();
-            using var cryptoStream = new CryptoStream(outputFileStream, aes.CreateEncryptor(Encoding.UTF8.GetBytes(key), aes.IV), CryptoStreamMode.Write);
+	/// <summary>
+	/// Encrypt the file of "inputFilePath" and output to the location of "outputFilePath".
+	/// </summary>
+	/// <param name="inputFilePath"></param>
+	/// <param name="outputFilePath"></param>
+	/// <returns>Returns true if the encryption was successful.</returns>
+	public static bool Encrypt(string keyFile, string inputFilePath, string outputFilePath)
+	{
+		try
+		{
+			if (!Path.Exists(keyFile))
+			{
+				Console.WriteLine("ERROR!!");
+				Console.WriteLine("keyFile not found.");
+				return false;
+			}
 
-            byte[] dummy = new byte[16];
-            await cryptoStream.WriteAsync(dummy, 0, 16);
+			string jsonString = File.ReadAllText(keyFile);
+			Key? key = JsonSerializer.Deserialize<Key>(jsonString);
+			if (key == null)
+			{
+				Console.WriteLine("ERROR!!");
+				Console.WriteLine("key is null.");
+				return false;
+			}
+			using var inputFileStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
+			using var outputFileStream = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write);
+			using var aes = Aes.Create();
+			using var cryptoStream = new CryptoStream(outputFileStream, aes.CreateEncryptor(Encoding.UTF8.GetBytes(key.Value), aes.IV), CryptoStreamMode.Write);
 
-            byte[] buffer = new byte[8192];
-            int len = 0;
-            while((len = await inputFileStream.ReadAsync(buffer, 0, 8192)) > 0)
-            {
-                await cryptoStream.WriteAsync(buffer, 0, len);
-            }
+			byte[] dummy = new byte[16];
+			cryptoStream.Write(dummy, 0, 16);
 
-            return true;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("ERROR!!");
-            Console.WriteLine(e.Message);
-            Console.WriteLine(e.StackTrace);
-            return false;
-        }
-    }
+			byte[] buffer = new byte[8192];
+			int len = 0;
+			while ((len = inputFileStream.Read(buffer, 0, 8192)) > 0)
+			{
+				cryptoStream.Write(buffer, 0, len);
+			}
 
-    /// <summary>
-    /// Decrypts the encrypted file at "inputFilePath" and returns it as a byte array.
-    /// </summary>
-    /// <param name="inputFilePath"></param>
-    /// <returns></returns>
-    public static async Task<byte[]?> Decrypt(string key, string inputFilePath)
-    {
-        try
-        {
-            using var aes = Aes.Create();
-            using var inputFileStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
-            using var memoryStream = new MemoryStream();
-            using var cryptoStream = new CryptoStream(inputFileStream, aes.CreateDecryptor(Encoding.UTF8.GetBytes(key), aes.IV), CryptoStreamMode.Read);
+			return true;
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine("ERROR!!");
+			Console.WriteLine(e.Message);
+			Console.WriteLine(e.StackTrace);
+			return false;
+		}
+	}
 
-            byte[] dummy = new byte[16];
-            await cryptoStream.ReadAsync(dummy, 0, 16);
+	/// <summary>
+	/// Decrypts the encrypted file at "inputFilePath" and returns it as a byte array.
+	/// </summary>
+	/// <param name="inputFilePath"></param>
+	/// <returns></returns>
+	public static byte[]? Decrypt(string key, string inputFilePath)
+	{
+		try
+		{
+			using var aes = Aes.Create();
+			using var inputFileStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
+			using var memoryStream = new MemoryStream();
+			using var cryptoStream = new CryptoStream(inputFileStream, aes.CreateDecryptor(Encoding.UTF8.GetBytes(key), aes.IV), CryptoStreamMode.Read);
 
-            byte[] buffer = new byte[8192];
-            int len = 0;
-            while ((len = await cryptoStream.ReadAsync(buffer, 0, 8192)) > 0)
-            {
-                await memoryStream.WriteAsync(buffer, 0, len);
-                await memoryStream.FlushAsync();
-            }
+			byte[] dummy = new byte[16];
+			cryptoStream.Read(dummy, 0, 16);
 
-            return memoryStream.ToArray();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("ERROR!!");
-            Console.WriteLine(e.Message);
-            Console.WriteLine(e.StackTrace);
-            return null;
-        }
-    }
+			byte[] buffer = new byte[8192];
+			int len = 0;
+			while ((len = cryptoStream.Read(buffer, 0, 8192)) > 0)
+			{
+				memoryStream.Write(buffer, 0, len);
+				memoryStream.FlushAsync();
+			}
+
+			return memoryStream.ToArray();
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine("ERROR!!");
+			Console.WriteLine(e.Message);
+			Console.WriteLine(e.StackTrace);
+			return null;
+		}
+	}
+
+	public class Key
+	{
+		public string Value { get; set; } = "";
+	}
 }
--- src/Program.cs
+++ src/Program.cs
@@ -2,70 +2,85 @@
 
 class Program
 {
-    static async Task Main(string[] args)
-    {
-        if (args.Length != 4)
-        {
-            Console.WriteLine("Usage: FileEncryptor.dll <inputDirectoryPath> <outputDirectoryPath> <key> <fileExtension>");
-            return;
-        }
+	static async Task Main(string[] args)
+	{
+		try
+		{
+			if (args.Length != 4)
+			{
+				Console.WriteLine("Usage: FileEncryptor.dll <inputDirectoryPath> <outputDirectoryPath> <key> <fileExtension>");
+				return;
+			}
 
-        var inputDirectoryPath = args[0];
-        var outputDirectoryPath = args[1];
+			var inputDirectoryPath = args[0];
+			var outputDirectoryPath = args[1];
 
-        if (!Directory.Exists(inputDirectoryPath))
-        {
-            Console.WriteLine("ERROR.");
-            Console.WriteLine("argument 1(inputPath): Directory not found.");
-            return;
-        }
+			if (!Directory.Exists(inputDirectoryPath))
+			{
+				Console.WriteLine("ERROR.");
+				Console.WriteLine("argument 1(inputPath): Directory not found.");
+				Console.WriteLine(inputDirectoryPath);
+				return;
+			}
 
-        if (!Directory.Exists(outputDirectoryPath))
-        {
-            Console.WriteLine("ERROR.");
-            Console.WriteLine("argument 2(outputPath): Directory not found.");
-            return;
-        }
+			if (!Directory.Exists(outputDirectoryPath))
+			{
+				Directory.CreateDirectory(outputDirectoryPath);
+			}
 
-        var key = args[2];
-        var fileExtension = args[3];
+			var keyFileName = args[2];
+			var fileExtension = args[3];
 
-        if (key == "")
-        {
-            Console.WriteLine("ERROR.");
-            Console.WriteLine("argument 3(key): key is empty.");
-            return;
-        }
+			if (keyFileName == "")
+			{
+				Console.WriteLine("ERROR.");
+				Console.WriteLine("argument 3(keyFileName): Name is empty.");
+				return;
+			}
 
-        if (fileExtension == "")
-        {
-            Console.WriteLine("ERROR.");
-            Console.WriteLine("argument 3(key): fileExtension is empty.");
-            return;
-        }
+			if (fileExtension == "")
+			{
+				Console.WriteLine("ERROR.");
+				Console.WriteLine("argument 4(fileExtension): FileExtension is empty.");
+				return;
+			}
 
-        // All files existing in the directory of Path passed in argument 1 are encrypted into *.asset files and output to the position of Path of argument 2.
-        foreach (var file in new DirectoryInfo(inputDirectoryPath).GetFiles("*", SearchOption.AllDirectories))
-        {
-            // Get output file path.
-            var outputFilePath = file.FullName.Replace(inputDirectoryPath, outputDirectoryPath);
-            outputFilePath = outputFilePath.Remove(outputFilePath.LastIndexOf("."), outputFilePath.Length - outputFilePath.LastIndexOf(".")) + $".{fileExtension}";
+			inputDirectoryPath = inputDirectoryPath.Replace("/", @"\");
+			outputDirectoryPath = outputDirectoryPath.Replace("/", @"\");
 
-            // Get output directory.
-            var outputDirectory = outputFilePath.Remove(outputFilePath.LastIndexOf(@"\"), outputFilePath.Length - outputFilePath.LastIndexOf(@"\"));
+			// All files existing in the directory of Path passed in argument 1 are encrypted into *.asset files and output to the position of Path of argument 2.
+			foreach (var file in new DirectoryInfo(inputDirectoryPath).GetFiles("*", SearchOption.AllDirectories))
+			{
+				if (file.FullName.IndexOf(inputDirectoryPath) == -1) continue;
 
-            // Create directory.
-            if (!Directory.Exists(outputDirectory)) Directory.CreateDirectory(outputDirectory);
+				// Get output file path.
+				var outputFilePath = file.FullName.Replace(inputDirectoryPath, outputDirectoryPath);
+				outputFilePath = outputFilePath.Remove(outputFilePath.LastIndexOf("."), outputFilePath.Length - outputFilePath.LastIndexOf(".")) + $".{fileExtension}";
 
-            // File encrypt.
-            var result = await Encryptor.Encrypt(key, file.FullName, outputFilePath);
-            if (!result)
-            {
-                Console.WriteLine("Encryption did not complete successfully due to an error.");
-                return;
-            }
-        }
+				// Get output directory.
+				var outputDirectory = outputFilePath.Remove(outputFilePath.LastIndexOf(@"\"), outputFilePath.Length - outputFilePath.LastIndexOf(@"\"));
 
-        Console.WriteLine("Encryption successfully completed.");
-    }
+				// Create directory.
+				if (!Directory.Exists(outputDirectory)) Directory.CreateDirectory(outputDirectory);
+
+				// File encrypt.
+				var result = await Task.Factory.StartNew(() =>
+				{
+					return Encryptor.Encrypt(keyFileName, file.FullName, outputFilePath);
+				});
+
+				if (!result)
+				{
+					Console.WriteLine("Encryption did not complete successfully due to an error.");
+					return;
+				}
+			}
+
+			Console.WriteLine("Encryption successfully completed.");
+		}
+		catch
+		{
+			throw;
+		}
+	}
 }
